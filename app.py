e5f1089f16148214e738e3cd12137f16a8eaf48a
Anirban <anirbandaskvch@gmail.com>
2023-04-01 11:52:14 +0000

    added dynamic settings

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'data analyst',
+  'location': 'Delhi, India',
+  'salary': 'Rs. 10,00,000'
+}, {
+  'id': 1,
+  'title': 'data scientist',
+  'location': 'remote',
+}, {
+  'id': 1,
+  'title': 'frontend',
+  'location': 'bangaluru, India',
+  'salary': 'Rs. 12,00,000'
+}, {
+  'id': 1,
+  'title': 'backend',
+  'location': 'San Francisco, USA',
+  'salary': '$120,000'
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company='Jovian')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
